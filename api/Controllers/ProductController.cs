104af7cf0fd5af17a945ec167f6f26f4de8f8d33
Joao Cesar Rizzatto de Freitas <joaocesar.rizzatto85@gmail.com>
2025-02-28 15:46:22 -0300

    product controller

---
+++ api/Controllers/ProductController.cs
@@ -0,0 +1,111 @@
+﻿using Domain.Entities;
+using Domain.Interfaces.Services;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductController : ControllerBase
+    {
+        private readonly IProductService _productService;
+
+        public ProductController(IProductService productService)
+        {
+            _productService = productService;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetProducts([FromQuery] int page = 1, [FromQuery] int size = 10, [FromQuery] string order = "")
+        {
+            try
+            {
+                var products = await _productService.GetProductsWithTotalCountAsync(page, size, order);
+                return Ok(new
+                {
+                    Data = products,
+                    TotalItems = products.TotalItems,
+                    CurrentPage = page,
+                    TotalPages = (int)Math.Ceiling(products.TotalItems / (double)size)
+                });
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+
+        [HttpGet("GetProductById")]
+        public async Task<IActionResult> GetProductById([FromQuery] Guid id)
+        {
+            try
+            {
+                var product = await _productService.GetProductById(id);
+                if (product == null)
+                    return NotFound();
+                return Ok(product);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> AddProduct([FromBody] Product product)
+        {
+            try
+            {
+                var createdProduct = await _productService.AddProductAsync(product);
+                return Ok(createdProduct);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+
+        [HttpPut("UpdateProduct")]
+        public async Task<IActionResult> UpdateProduct([FromBody] Product product)
+        {
+            try
+            {
+                await _productService.UpdateProductAsync(product);
+                return Ok();
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+
+        [HttpDelete("DeleteProduct")]
+        public async Task<IActionResult> DeleteProduct([FromQuery] Guid id)
+        {
+            try
+            {
+                await _productService.DeleteProductAsync(id);
+                return Ok();
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+
+        [HttpGet("categories")]
+        public async Task<IActionResult> GetCategories()
+        {
+            try
+            {
+                var categories = await _productService.GetCategoriesAsync();
+                return Ok(categories);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+    }
+}
